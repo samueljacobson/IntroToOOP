fdf84ab151e32f32bb63d89405adbef56b1f5271
Sam Jacobson <samuel.c.jacobson@gmail.com>
2017-07-11 10:46:46 -0400

    added more to cat and program

--- IntroToOOP/Cat.cs
+++ IntroToOOP/Cat.cs
@@ -20,11 +20,16 @@ namespace IntroToOOP
         private bool isHungry = true;
 
         //Properties - mix between a field and method that allow controlled access
-        public string Name
+        public string Name          //default setup
             {
-                get {return this.name;}
-                set { this.name = value; }
+                get {return this.name;}  //get returns the associated value, this.name points to object
+                set {this.name = value;}
             }
+
+        public int Age
+        {
+            get { return this.age; }  //in properties ; inside {}
+        }
 
         //Behaviors:
         //Methods - repeatable/resuable sections of code
@@ -33,9 +38,11 @@ namespace IntroToOOP
         //  - allow us to control the state of an object upon creation
         //  - allow us to perform any actions the object might need upon creation
         //  - always share the name of the class
+        //  - can have as many constructors as needed, as long as each have a unique signature (i.e. parameters in same data type order)
         public Cat()
         {
             //This is an example of a default constructor. A default constructor takes not arguments or parameters
+            //Create oject without setting any values to fields
         }
 
         public Cat(string name, int age, string furColor)
